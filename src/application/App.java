978e860bda8bd0e08838302d0b470ea164d77902
Derivaldo Brito <derivaldo.brito@yahoo.com.br>
2023-01-13 11:34:30 -0300

    first commit

---
+++ src/application/App.java
@@ -0,0 +1,12 @@
+package application;
+
+import entities.BusinessAccount;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        BusinessAccount account = new BusinessAccount();
+
+        account.getBalance();
+    }
+}
